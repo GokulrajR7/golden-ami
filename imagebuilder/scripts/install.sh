c46dc5afe1a72c61a9312469e32a5475601153b2
GokulrajR7 <gokulrajeswaran2005@gmail.com>
2026-05-17 11:04:09 +0530

    Update install.sh

--- imagebuilder/scripts/install.sh
+++ imagebuilder/scripts/install.sh
@@ -3,26 +3,24 @@
 set -e
 
 echo "Updating system..."
-sudo dnf update -y
+dnf update -y
 
 echo "Installing basic tools..."
-sudo dnf install -y git wget unzip tar
+dnf install -y git wget unzip tar
 
 echo "Installing Docker..."
-sudo dnf install -y docker
-sudo systemctl enable docker
-sudo systemctl start docker
-sudo usermod -aG docker ec2-user
+dnf install -y docker
+
+systemctl enable docker
+systemctl start docker
+
+usermod -aG docker ec2-user
 
 echo "Installing Java 21..."
-sudo dnf install -y java-21-amazon-corretto
-
-# Optional NodeJS
-# sudo dnf module enable nodejs:20 -y
-# sudo dnf install -y nodejs
+dnf install -y java-21-amazon-corretto
 
 echo "Cleaning packages..."
-sudo dnf clean all
+dnf clean all
 
 echo "Validation..."
 
